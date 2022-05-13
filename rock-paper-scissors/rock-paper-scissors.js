ad625f677575810e97cec1ea1e03b0ddff2264e1
Steven Jarmell <sjj27@pitt.edu>
2022-05-13 15:25:20 -0400

    Finished back-end

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,3 +1,7 @@
+let LOSE = 0;
+let WIN = 1;
+let TIE = 2;
+
 let computerPlay = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) return 'Rock';
@@ -10,39 +14,70 @@ let playRound = (playerSelection, computerSelection) => {
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return 'You tied!';
+        return TIE;
     }
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            return 'You Lose! Paper beats rock';
+            return LOSE;
         }
         else {
-            return 'You Win! Rock beats Scissors';
+            return WIN;
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
-            return 'You Lose! Scissors beats paper';
+            return LOSE;
         }
         else {
-            return 'You Win! Paper beats rock';
+            return WIN;
         }
     }
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            return 'You Lose! Rock beats scissors';
+            return LOSE;
         }
         else {
-            return 'You Win! Scissors beats paper';
+            return WIN;
         }
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let userInput = prompt("Enter rock, paper, or scissors: ");
-        console.log(playRound(userInput, computerPlay()));
+const buttons = document.querySelectorAll('button');
+let computerScore = document.getElementById('computer-score');
+let humanScore = document.getElementById('human-score');
+// If the user clicks a button, play a round and update the score
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let round = playRound(button.id, computerPlay());
+        
+        // Case 1: User loses to computer
+        if (!round) {
+            increaseScore(computerScore);
+        }
+        // Case 2: User beats computer
+        else if (round === WIN) {
+            increaseScore(humanScore);
+        }
+    });
+});
+
+function increaseScore(element) {
+    let val = element.innerHTML;
+    val++;
+    element.innerHTML = val;
+
+    // Ends game if one player reaches 5
+    if (element.innerHTML == 5) {
+        buttons.forEach((button) => {
+            button.disabled = true;
+        });
+
+        if (element === computerScore) {
+            alert("You lose!");
+        }
+        else {
+            alert("You win!");
+        }
     }
 }
-
-game();
+    
