f88ef5ade5a57aa8125dacf7f418c1e61aa494db
Steven Jarmell <sjj27@pitt.edu>
2022-05-14 11:45:11 -0400

    Added the draw feature in one color

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,4 +1,29 @@
-const containerDiv = document.createElement('div');
-containerDiv.classList('grid-container');
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach(div => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    let amount = size * size;
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor='black';
+        });
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement('beforeend', square);
+    }
+}
 
+populateBoard(16);
+
+function changeSize(input) {
+    if (input > 100|| input <= 2) {
+        console.log("Invalid input");
+    }
+    else {
+        populateBoard(input);
+        console.log("Valid input");
+    }
+}
