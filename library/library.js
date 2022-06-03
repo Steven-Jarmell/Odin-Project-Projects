e99ac091d4f07c04feb1d73f1b8bf6ffb50a7c5f
Steven Jarmell <sjj27@pitt.edu>
2022-06-03 17:03:41 -0400

    Init JS work

--- library/library.js
+++ library/library.js
@@ -1,1 +1,29 @@
-console.log("Init");
+let myLibrary = [
+    {
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        numPages: 154,
+        read: true,
+    }
+];
+
+function Book(title, author, numPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    //
+}
+
+let formData = document.querySelector('#book-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    let formElements = new FormData(event.target);
+    for (let element in formElements) {
+        console.log(element.keys);
+    }
+});
+
+console.log("Running");
