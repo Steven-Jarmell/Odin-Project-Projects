714182b31f2abecc53a04a90083914d8b34af269
Steven Jarmell <sjj27@pitt.edu>
2022-06-06 17:23:34 -0400

    Changed HTML to include a message at the bottom and finished game functionality in JavaScript

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -3,17 +3,37 @@ const GameBoard = (() => {
     board = ['','','','','','','','',''];
 
     let boardSquares = document.querySelector('.gameboard');
-
-    board.forEach((marker) => {
+    board.forEach((marker, index) => {
         const square = document.createElement('div');
         square.className = 'square';
-        square.innerHTML = marker;
         boardSquares.appendChild(square);
-
         square.addEventListener('click', () => {
+            if(game.gameEnded) return;
             square.innerHTML = game.activePlayer.symbol;
+            board[index] = game.activePlayer.symbol;
+            square.style.pointerEvents = 'none';
+
+            let winner = game.checkWin();
+            game.decreaseRoundsRemaining();
+            
+            // Win
+            if (winner) {
+                let footer = document.querySelector('.footer');
+                footer.innerHTML = `${game.activePlayer.name} wins!`;
+            }
+            // If not a win, check for a tie
+            else if (game.getRoundsRemaining() === 0) {
+                let footer = document.querySelector('.footer');
+                footer.innerHTML = 'Tie';
+            }
+            
             game.changePlayer();
-        })
+
+            let playerName = document.querySelector('.player-name');
+            playerName.innerHTML = game.activePlayer.name;
+        
+        });
+
     });
 
     return {board};
@@ -29,12 +49,58 @@ const game = (() => {
     const playerTwo = Player('Player 2', 'O');
 
     let activePlayer = playerOne;
+    let roundsRemaining = 9;
+    let gameEnded = false;
     
     function changePlayer() {
         this.activePlayer === playerOne ? this.activePlayer = playerTwo : this.activePlayer = playerOne;
     }
 
+    function decreaseRoundsRemaining() {
+        roundsRemaining -= 1;
+        if (roundsRemaining === 0) {
+            this.gameEnded = true;
+        }
+    }
+
+    function getRoundsRemaining() {
+        return roundsRemaining;
+    }
+
+    // Shoutout stackoverflow for the naiive method
+    const winningPositions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    function checkWin() {
+        if (roundsRemaining >= 6) {
+            return false;
+        }
+        else {
+            for (let i = 0; i < winningPositions.length; i++) {
+                let curRow = winningPositions[i];
+                if (GameBoard.board[curRow[0]] === this.activePlayer.symbol && GameBoard.board[curRow[1]] === this.activePlayer.symbol && GameBoard.board[curRow[2]] === this.activePlayer.symbol) {
+                    this.gameEnded = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
     return {
         activePlayer,
-        changePlayer}
+        changePlayer,
+        checkWin,
+        decreaseRoundsRemaining,
+        getRoundsRemaining,
+        gameEnded
+    };
 })();
