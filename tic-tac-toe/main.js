38712ca3a9cd58b4185151cd686bf1429673e0f8
Steven Jarmell <sjj27@pitt.edu>
2022-06-06 15:36:20 -0400

    Set up initial board

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,1 +1,19 @@
-alert("linked");
+// Gameboard Module
+const GameBoard = (() => {
+    board = ['X','O','X','O','X','O','X','O','X'];
+
+    let boardSquares = document.querySelector('.gameboard');
+
+    board.forEach((marker) => {
+        const square = document.createElement('div');
+        square.className = 'square';
+        square.innerHTML = marker;
+        boardSquares.appendChild(square);
+    });
+
+    function placeMarker(marker, index) {
+        board[index] = marker;
+    };
+
+    return {board};
+})();
